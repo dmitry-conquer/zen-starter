9c3283449ddd9b9023726e5304c4460ba91b3ecd
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-07-06 09:39:11 +0300

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+          grid: true,
+          overrideBrowserslist: "last 2 versions",
+  },
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+   content: [
+    "./index.html",
+    "./pages/**/*",
+    "./partials/**/*",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
