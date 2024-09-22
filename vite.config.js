14828d207249d326c782de57d687c23a93e97ad9
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-09-22 11:50:06 +0300

    Stylelint + minor updates

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
-import viteJoinMediaQueries from 'vite-join-media-queries';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import viteJoinMediaQueries from "vite-join-media-queries";
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   build: {
     modulePreload: false,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'pages/index.html'),
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "pages/index.html"),
       },
       output: {
         assetFileNames: ({ name }) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-            return 'assets/images/[name]-[hash][extname]';
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
           }
-          return 'assets/[name]-[hash][extname]';
+          return "assets/[name]-[hash][extname]";
         },
       },
     },
@@ -24,7 +31,7 @@ export default defineConfig({
   plugins: [
     viteJoinMediaQueries(),
     handlebars({
-      partialDirectory: resolve(__dirname, 'partials'),
+      partialDirectory: resolve(__dirname, "partials"),
     }),
   ],
 });
