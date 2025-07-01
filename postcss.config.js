ceb93ae9f6da1cd25473e1d7cfd1e412a307addd
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-07-01 08:23:29 +0300

    chore: update version to 1.4.5 and remove unused dependencies
    - Bump package version from 1.4.4 to 1.4.5 in package.json
    - Remove @fullhuman/postcss-purgecss from package.json and package-lock.json
    - Remove purgeCSSPlugin from postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
-import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
 import autoprefixer from "autoprefixer";
 import postcssCombineMediaQuery from "postcss-combine-media-query";
 import postcssSortMediaQueries from "postcss-sort-media-queries";
 
 export default {
   plugins: [
-    purgeCSSPlugin({
-      content: ["./pages/**/*.html", "./components/**/*.html"],
-    }),
     postcssCombineMediaQuery,
     postcssSortMediaQueries,
     autoprefixer({
